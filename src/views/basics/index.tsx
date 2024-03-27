4c59b1e3bfbf0fb404a56941354d15b370a08c9d
Andy Nelson <jenkins-x@googlegroups.com>
2024-03-27 14:16:17 +0000

    learn: completed excercise

--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -29,7 +29,18 @@ export const BasicsView: FC = ({ }) => {
         network == 'mainnet-beta' ? process.env.NEXT_PUBLIC_MAINNET_RPC! :
           clusterApiUrl(network)
 
-    // Fetch for the Manifest
+    // Constructing our DexterityWallet from the Solana Wallet Adapter
+    const wallet: DexterityWallet = {
+      publicKey: publicKey!,
+      signTransaction,
+      signAllTransactions,
+    }
+
+    // Fetching the Manifest
+    const manifest = await dexterity.getManifest(rpc, true, wallet)
+
+    // Setting our Manifest with our Global Context
+    setManifest(manifest)
 
   }, [publicKey, network]);
 
@@ -44,6 +55,7 @@ export const BasicsView: FC = ({ }) => {
         <div className="text-center">
           <DefaultInfo />
           <SelectTraderAccounts />
+          {trader && (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-4">
               <div className="col-span-1 md:col-span-1 lg:col-span-1">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -51,7 +63,7 @@ export const BasicsView: FC = ({ }) => {
                     <PlaceLimitOrder />
                   </div>
                   <div>
-                  <FundingTrader />
+                    <FundingTrader />
                   </div>
                 </div>
                 <div className="mt-4"><OpenOrders /></div>
@@ -60,6 +72,8 @@ export const BasicsView: FC = ({ }) => {
                 <div className="mt-4"><AccountInfo /></div>
               </div>
             </div>
+
+          )}
         </div>
       </div>
     </div>
